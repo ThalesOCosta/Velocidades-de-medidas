29c6f672cb7c409733d4ab90073f596d8ad9a081
ThalesOCosta <105451422+ThalesOCosta@users.noreply.github.com>
2022-05-12 16:17:29 -0300

    Add files via upload

---
+++ velocidades e medidas.py
@@ -0,0 +1,17 @@
+# Km/h - M/s
+K = float(input('Km/h: '))
+M = K / 3.6
+print(f'M/s = {M}')
+
+M = float(input('M/s: '))
+K = M * 3.6
+print(f'Km/h = {K}')
+
+# Metro para milhas
+Mi = float(input('Milhas: '))
+K = 1.61 * Mi
+print(f'Km = {K}')
+
+K = float(input('Km: '))
+Mi = K/1.61
+print(f'Milhas: {Mi}')
